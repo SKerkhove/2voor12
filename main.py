b75212f86f6451b751abccf73d07a890ef123c23
SKerkhove <sylviakerkhove@gmail.com>
2024-02-14 17:20:40 +0100

    added more words

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import random
 from random import randint
 
 
-def readwords():
+def readwords(length):
     words = []
-    with open('8letterwords.txt', 'r') as file:
-        # reading each line
-        for line in file:
-            words = words + line.split()
+    if length == 8:
+        with open('8letterwords.txt', 'r') as file:
+            # reading each line
+            for line in file:
+                words = words + line.split()
+        return words
+    if length == 12:
+        with open('12letterwords.txt', 'r') as file:
+            # reading each line
+            for line in file:
+                words = words + line.split()
+        return words
+    else:
+        print("No valid word length")
     return words
 
 
@@ -60,24 +71,61 @@ def paardensprong(words):
     makePaardensprong(letters)
     return word
 
+
+def eindspel(words):
+    word = pickword(words)
+    letters = list(word)
+    random.shuffle(letters)
+    print("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+    print(letters[0], letters[1], letters[2], letters[3], letters[4], letters[5],letters[6],letters[7], letters[8], letters[9]," ",letters[10],letters[11])
+    print("_ _ _ _ _ _ _ _ _ _ _ _")
+
+    str = []
+    while True:
+        ans2 = input("Typ het nummer van de letter die je wilt zien")
+        l = letters[int(ans2) -1]
+        for i in range(12):
+            print(str)
+            if i == list(word).index(l):
+                str[i] = l + " "
+            elif len(str)<12:
+                # str.append(str + "_ ")
+                str.append(i)
+        print(str)
+        print("".join(str))
+
+    return word, letters
+
+
 if __name__ == '__main__':
-    words = readwords()
-    ans1 = input("Welk spel wil je spelen?"
-                 "(0) Taartpuzzel"
-                 "(1) Paardensprong")
+
+    ans1 = input("Welk spel wil je spelen?\n"
+                 "(0) Taartpuzzel\n"
+                 "(1) Paardensprong\n"
+                 "(2) Eindwoord")
 
     while True:
         if int(ans1) == 0:
+            words = readwords(8)
             word, missing_letter = taart(words)
 
             ans = input("Druk op enter voor de oplossing")
             if ans != None:
                 print("De ontbrekende letter was ", missing_letter,"\n", word)
         if int(ans1) == 1:
+            words = readwords(8)
             word = paardensprong(words)
             ans = input("Druk op enter voor de oplossing")
             if ans != None:
                 print("Het antwoord is ", word)
+        if int(ans1) == 2:
+            print("Spel niet beschikbaar nu")
+            break
+            # words = readwords(12)
+            # word, _ = eindspel(words)
+            # ans = input("Druk op enter voor de oplossing")
+            # if ans != None:
+            #     print("Het antwoord is ", word)
 
         again = input("Nieuwe puzzel?"
                       "\n\t(0) Ja"
